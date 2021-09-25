39739ddf6bc712ef72d86de9aa4d2095d9a8179c
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-25 11:33:14 +0000

    First block solved

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 ############DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+# Describe Problem
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
+  #function is suppoed to print 20 only when i = 20, but range() won't ever go to 20
 
 # # Reproduce the Bug
 # from random import randint
