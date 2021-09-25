86e02d56830150b9e6ad647934f9946dabc19dc9
Alex Smith <54478329+alexsmith922@users.noreply.github.com>
2021-09-25 11:39:52 +0000

    fixed block 3

--- main.py
+++ main.py
@@ -8,19 +8,20 @@
 # my_function()
 #   #function is suppoed to print 20 only when i = 20, but range() won't ever go to 20
 
-# # Reproduce the Bug
-from random import randint
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5)
-print(dice_imgs[dice_num])
-  #randint() was occasionally returning 6, which is out of range of dice_imgs
+# # # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(0, 5)
+# print(dice_imgs[dice_num])
+#   #randint() was occasionally returning 6, which is out of range of dice_imgs
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# Play Computer
+year = int(input("What's your year of birth?: "))
+if year > 1980 and year <= 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
+  #code block did not account for someone that was born in 1994. Added <= to line 20
 
 # # Fix the Errors
 # age = input("How old are you?")
